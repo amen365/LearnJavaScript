b901089b95faa2f67c405e412d80e300c27dee9a
amen365 <60744064+amen365@users.noreply.github.com>
2022-07-27 12:27:56 -0400

    Update kelvinProject.js

--- kelvinProject.js
+++ kelvinProject.js
@@ -1,1 +1,13 @@
+/* convert kelvin to celsius then to fahrenheit */
 
+const kelvin = 293;
+//const means it will stay the same value and be unable to be reassigned
+console.log(kelvin);
+let celsius = kelvin - 273;
+//celcius is kelvin (already determinded) - 273
+//console.log(celsius); -> check celsius
+let fahrenheit = celsius * (9/5) + 32;
+//formula was given
+fahrenheit = Math.floor(fahrenheit);
+//this will round down the temperature
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
